871dfcf030af12f85af014214be9ef0d0634df42
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-10-24 09:58:18 +0300

    feat: Add Playwright Test DocType and related files

---
+++ playwright/playwright/doctype/playwright_test/playwright_test.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Livuza and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PlaywrightTest(Document):
+	pass
